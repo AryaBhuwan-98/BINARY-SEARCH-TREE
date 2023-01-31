62d6ed1fe8c18bd45403361c494b42d1a350697e
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-01-30 20:53:16 -0800

    Add:Welcome to the Binary Search Tree Program

---
+++ src/main/java/com/bridgelabz/binarysearchtree/App.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.binarysearchtree;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to the Binary Search Tree" );
+    }
+}
